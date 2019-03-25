60fa0ea8e774ad3f81028b2853f622e99db6bdd5
Mannan Qazi <mannanqazi2@gmail.com>
2019-03-24 22:40:23 -0400

    user cannot press equal again without selecting an operator first

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,11 @@
 // when suer clicks a number button, the value gets populated to the display div. when user clicks operator functions, a second number starts to get populated. Then when they click "=", two numbers are operated accordingly.
 
 $(document).ready(function() {
-  var num1, num2, val;
+  var num1, num2, operatorChosen, operatorChosenRepeat;
 
   num1 = 0;
-  //num2 = 0;
+  num2 = 0;
+  operatorChosen = '';
 
   $('.number').on('click', function(val) {
     var disp_val = $(this).val();
@@ -12,16 +13,47 @@ $(document).ready(function() {
   });
 
   $('.operator').on('click', function() {
-    num1 = document.querySelector('.display').innerHTML;
+    // check which operator is being clicked:
+    operatorChosen = $(this).attr('id');
+    operatorChosenRepeat = $(this).attr('id');
+
+    console.log(operatorChosen);
+
+    // store the number entered into num1 variable
+    num1 = parseInt(document.querySelector('.display').innerHTML);
+
+    // and then empty the screen for entering the next number
+    $('.display').html('');
+
+    //console.log(num1);
   });
 
   $('#clear').on('click', function() {
+    num1 = 0;
+    num2 = 0;
+    operatorChosen = '';
     $('.display').text('');
   });
 
   $('#operate').on('click', function() {
-    operate(num1, num2);
+    if (operatorChosen === '') {
+      // operatorChosen = operatorChosenRepeat;
+      // // current = num2;
+      // operate(num1, num2, operatorChosen);
+      alert('chose operator');
+    } else {
+      var current = parseInt(document.querySelector('.display').innerHTML);
+      console.log(current);
+      num2 = current;
+
+      //console.log(typeof num2);
+      console.log(typeof current);
+      operate(num1, current, operatorChosen);
+      //console.log(num1);
+      operatorChosen = '';
+    }
   });
+
   function add(a, b) {
     return a + b;
   }
@@ -35,10 +67,28 @@ $(document).ready(function() {
   }
 
   function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+      return 'oops! b cannot be 0';
+    } else {
+      return a / b;
+    }
   }
 
-  // function operate(a, b, action) {
-  //   if(action === 'add')
-  // }
+  function operate(number1, number2, operatorChosen) {
+    if (operatorChosen === 'add') {
+      num1 = add(number1, number2);
+      $('.display').text(num1);
+      //operatorChosen = '';
+      //console.log(operatorChosen);
+    } else if (operatorChosen === 'subtract') {
+      $('.display').text(subtract(number1, number2));
+      //operatorChosen = '';
+    } else if (operatorChosen === 'multiply') {
+      $('.display').text(multiply(number1, number2));
+      //operatorChosen = '';
+    } else if (operatorChosen === 'divide') {
+      $('.display').text(divide(number1, number2));
+      //operatorChosen = '';
+    }
+  }
 });
